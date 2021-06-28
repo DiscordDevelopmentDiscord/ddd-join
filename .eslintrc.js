ebad5012895914190f18917a423ac86438e79832
Zaid "Nico <me@zaid-arshad.tech>
2021-06-27 20:27:43 -0400

    chore: appease the eslint gods

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
     extends: "marine/prettier/node",
     parserOptions: {
         "project": "./tsconfig.eslint.json"
+    },
+    rules: {
+        "@typescript-eslint/naming-convention": "off"
     }
 }
--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
-  module.exports = {
+module.exports = {
     semi: true,
     trailingComma: 'all',
     printWidth: 150,
