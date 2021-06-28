f563c880c316e3a63a45cc9c85b12c48e92f0402
Zaid "Nico <me@zaid-arshad.tech>
2021-06-27 20:28:57 -0400

    refactor(*): abstract all fetch reqs to a function, general cleanup

---
+++ src/util.ts
@@ -0,0 +1,37 @@
+export const CONSTANTS = {
+	headers: {
+		urlencoded: {
+			'content-type': 'application/x-www-form-urlencoded',
+		},
+		json: {
+			'content-type': 'application/json',
+		},
+	},
+}
+
+export const UTILS = {
+	auth: (token: string, type = 'Bearer') => ({
+		authorization: `${type} ${token}`,
+	}),
+}
+
+export async function makeRequest<T extends Record<string, any>>(
+	route: string,
+	options: RequestInit,
+): makeRequestResponseTuple<T> {
+	const req = await fetch(route, options)
+	if (!req.ok) {
+		const parseErrorResponseBody = await req.json().catch(() => ({}))
+		throw new DiscordAPIError(parseErrorResponseBody.message, options.method!, route, req.status)
+	}
+
+	return [req.json() as Promise<T>, req]
+}
+
+export class DiscordAPIError extends Error {
+	public constructor(msg: string, public method: string, public path: string, public status: number) {
+		super(`[DiscordAPIERR:${status}:${method.toUpperCase()}] ${path} - ${msg}`)
+	}
+}
+
+export type makeRequestResponseTuple<T> = Promise<[Promise<T>, Response]>
